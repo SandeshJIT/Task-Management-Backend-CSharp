5a5a0a5ed8f1447c107ea9cb9e5d82b58c39ee1a
Sandesh S <hssandesh111@gmail.com>
2024-02-19 01:31:04 +0530

    Add validator unit tests

---
+++ src/Todo.API.UnitTests/TaskRequestValidatorTests.cs
@@ -0,0 +1,53 @@
+﻿using FluentValidation.TestHelper;
+using Todo.API.Model;
+using Todo.API.Validators;
+
+namespace Todo.API.UnitTests;
+public class TaskRequestValidatorTests
+{
+    private readonly TaskRequestValidator _validator;
+
+    public TaskRequestValidatorTests()
+    {
+        _validator = new TaskRequestValidator();
+    }
+
+    [Fact]
+    public void TaskName_ShouldNotBeEmpty()
+    {
+        // Arrange
+        var request = new TaskRequest { TaskName = string.Empty };
+
+        // Act
+        var result = _validator.TestValidate(request);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(task => task.TaskName);
+    }
+
+    [Fact]
+    public void TaskName_ShouldNotBeNull()
+    {
+        // Arrange
+        var request = new TaskRequest { TaskName = null };
+
+        // Act
+        var result = _validator.TestValidate(request);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(task => task.TaskName);
+    }
+
+    [Fact]
+    public void TaskName_ShouldNotExceedMaximumLength()
+    {
+        // Arrange
+        var request = new TaskRequest { TaskName = new string('a', 256) };
+
+        // Act
+        var result = _validator.TestValidate(request);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(task => task.TaskName);
+    }
+}
