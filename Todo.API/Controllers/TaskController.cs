d2a447a0ab260e5a9e34797d07ca9d1983facace
Sandesh S <hssandesh111@gmail.com>
2024-02-18 09:11:40 +0530

    Add project files.

---
+++ Todo.API/Controllers/TaskController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Todo.API.Model;
+
+namespace Todo.API.Controllers
+{
+    [Route("/api/todo-list/task")]
+    public class TaskController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Create([FromBody] IEnumerable<TaskRequest> tasklist)
+        {
+            foreach (var task in tasklist)
+            {
+                Console.WriteLine(task.TaskName);
+            }
+            return Ok();
+        }
+    }
+}
---
+++ Todo.API/Model/TaskRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Todo.API.Model
+{
+    public class TaskRequest
+    {
+        public string TaskName { get; set; }
+        public string? TaskDescription { get; set; }
+
+        public TaskRequest()
+        {
+            TaskName = string.Empty;
+        }
+    }
+}
